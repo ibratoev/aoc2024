fc4842abfff8b9c95e3fd45f940ec512e15c5753
Ivaylo Bratoev <ivaylo.bratoev@gmail.com>
2024-12-07 18:39:34 +0200

    Solution for challenge 2

--- challenge/day2/a.go
+++ challenge/day2/a.go
@@ -3,10 +3,12 @@ package day2
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/ibratoev/aoc2024/challenge"
+	"github.com/ibratoev/aoc2024/util"
 )
 
 func aCommand() *cobra.Command {
@@ -19,6 +21,44 @@ func aCommand() *cobra.Command {
 	}
 }
 
+// Checks if a report is safe. If the report is not safe, the index of the first failing item is returned.
+func isSafe(report []int) (bool, int) {
+	increasing := report[0] < report[1]
+
+	for i := 0; i < len(report)-1; i++ {
+		cur := report[i]
+		next := report[i+1]
+		diff := next - cur
+		if increasing {
+			if diff < 1 || diff > 3 {
+				return false, i
+			}
+		} else {
+			if diff < -3 || diff > -1 {
+				return false, i
+			}
+		}
+	}
+
+	return true, len(report) - 1
+}
+
 func partA(input io.Reader) int {
-	panic("Not implemented!")
+	sum := 0
+
+	for l := range challenge.Lines(input) {
+		fields := strings.Fields(l)
+
+		report := make([]int, len(fields))
+
+		for i, f := range fields {
+			report[i] = util.MustAtoI(f)
+		}
+
+		if isSafe, _ := isSafe(report); isSafe {
+			sum++
+		}
+	}
+
+	return sum
 }
--- challenge/day2/a_test.go
+++ challenge/day2/a_test.go
@@ -4,15 +4,29 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ibratoev/aoc2024/challenge"
 	"github.com/stretchr/testify/require"
 )
 
-func TestA(t *testing.T) {
-	t.Skip("Not Implemented")
+const testInput = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
 
-	input := strings.NewReader("foobar")
+func TestA(t *testing.T) {
+	input := strings.NewReader(testInput)
 
 	result := partA(input)
 
-	require.Equal(t, 42, result)
+	require.Equal(t, 2, result)
+}
+
+func TestAFull(t *testing.T) {
+	input := challenge.InputFile()
+
+	result := partA(input)
+
+	require.Equal(t, 564, result)
 }
