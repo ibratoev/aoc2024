79b04507426727e2c3929a761701a6bdc0549e82
Ivaylo Bratoev <ivaylo.bratoev@gmail.com>
2024-12-07 21:55:36 +0200

    Solution for challenge 3

--- challenge/day3/b.go
+++ challenge/day3/b.go
@@ -3,10 +3,12 @@ package day3
 import (
 	"fmt"
 	"io"
+	"regexp"
 
 	"github.com/spf13/cobra"
 
 	"github.com/ibratoev/aoc2024/challenge"
+	"github.com/ibratoev/aoc2024/util"
 )
 
 func bCommand() *cobra.Command {
@@ -20,5 +22,18 @@ func bCommand() *cobra.Command {
 }
 
 func partB(input io.Reader) int {
-	panic("Not implemented!")
+	sum := 0
+	enabled := true
+	mulRegex := regexp.MustCompile(`do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\)`)
+	for _, res := range mulRegex.FindAllStringSubmatch(challenge.Raw(input), -1) {
+		if res[0] == "do()" {
+			enabled = true
+		} else if res[0] == "don't()" {
+			enabled = false
+		} else if enabled {
+			sum += util.MustAtoI(res[1]) * util.MustAtoI(res[2])
+		}
+
+	}
+	return sum
 }
