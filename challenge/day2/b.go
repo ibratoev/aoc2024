25977ad8b953c7f926480d8938ec39d1907b71f7
Ivaylo Bratoev <ivaylo.bratoev@gmail.com>
2024-12-07 21:55:02 +0200

    Solution for challenge 2 (#5)

--- challenge/day2/b.go
+++ challenge/day2/b.go
@@ -3,10 +3,13 @@ package day2
 import (
 	"fmt"
 	"io"
+	"slices"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/ibratoev/aoc2024/challenge"
+	"github.com/ibratoev/aoc2024/util"
 )
 
 func bCommand() *cobra.Command {
@@ -19,6 +22,55 @@ func bCommand() *cobra.Command {
 	}
 }
 
+func isSafeB(report []int) bool {
+	safe, i := isSafe(report)
+
+	if safe {
+		return true
+	}
+
+	if i > 0 {
+		reportPrev := slices.Clone(report)
+		if safe, _ := isSafe(slices.Delete(reportPrev, i-1, i)); safe {
+			fmt.Println(safe)
+			fmt.Println(reportPrev)
+			return true
+		}
+	}
+
+	reportI := slices.Clone(report)
+	if safe, _ := isSafe(slices.Delete(reportI, i, i+1)); safe {
+		fmt.Println(safe)
+		fmt.Println(reportI)
+		return true
+	}
+
+	reportNext := slices.Clone(report)
+	if safe, _ := isSafe(slices.Delete(reportNext, i+1, i+2)); safe {
+		fmt.Println(safe)
+		fmt.Println(reportNext)
+		return true
+	}
+
+	return false
+}
+
 func partB(input io.Reader) int {
-	panic("Not implemented!")
+	sum := 0
+
+	for l := range challenge.Lines(input) {
+		fields := strings.Fields(l)
+
+		report := make([]int, len(fields))
+
+		for i, f := range fields {
+			report[i] = util.MustAtoI(f)
+		}
+
+		if isSafeB(report) {
+			sum++
+		}
+	}
+
+	return sum
 }
--- challenge/day2/b_test.go
+++ challenge/day2/b_test.go
@@ -4,15 +4,22 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ibratoev/aoc2024/challenge"
 	"github.com/stretchr/testify/require"
 )
 
 func TestB(t *testing.T) {
-	t.Skip("Not Implemented")
-
-	input := strings.NewReader("foobar")
+	input := strings.NewReader(testInput)
 
 	result := partB(input)
 
-	require.Equal(t, 42, result)
+	require.Equal(t, 4, result)
+}
+
+func TestBFull(t *testing.T) {
+	input := challenge.InputFile()
+
+	result := partB(input)
+
+	require.Equal(t, 604, result)
 }
