7d95961727a795657ac569d7962973f8c5d21e75
Ivaylo Bratoev <ivaylo.bratoev@gmail.com>
2025-01-09 21:02:52 +0200

    Day 6 solution (#10)
    * Day 6 solution

--- challenge/day6/a_test.go
+++ challenge/day6/a_test.go
@@ -4,15 +4,31 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ibratoev/aoc2024/challenge"
 	"github.com/stretchr/testify/require"
 )
 
 func TestA(t *testing.T) {
-	t.Skip("Not Implemented")
-
-	input := strings.NewReader("foobar")
+	input := strings.NewReader(`....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`)
 
 	result := partA(input)
 
-	require.Equal(t, 42, result)
+	require.Equal(t, 41, result)
+}
+
+func TestAFull(t *testing.T) {
+	input := challenge.InputFile()
+
+	result := partA(input)
+
+	require.Equal(t, 5095, result)
 }
